8286ec7fdc76e0e52a9e35560d4217aa8d1c3d99
Alex Zorg <azorg@mail.ru>
2015-08-26 21:12:49 +0300

    update _make.sh

--- _make.sh
+++ _make.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-test `uname` = "Linux" && OPT='-j2' || OPT='WIN32=1'
+if [ `uname` = "Linux" ]
+then
+  PROC_NUM=`grep processor /proc/cpuinfo | wc -l`
+	OPT="-j $PROC_NUM"
+else
+	OPT="WIN32=1"
+fi
 
 #mkdir -p _bin_release
 #mkdir -p _bin_debug
@@ -9,5 +15,5 @@ qmake qplot.pro
 #qmake qplot-qwt.pro
 #qmake qplot-win.pro
 
-make $OPT
+echo make $OPT
 
